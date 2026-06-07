b11aa921c01c083fca6cbbe8f601230398a87b45
A.R <104121680+bootless64@users.noreply.github.com>
2026-06-07 14:30:38 +0330

    Update ۱.py
    Co-authored-by: MahdiGhodsikhah <mahdi84m17@gmail.com>

--- ۱.py
+++ ۱.py
@@ -1,17 +1,4 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-# 1. بارگذاری تصویر (خاکستری)
-image_path = 'image.jpg'
-img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-
-# 2. Background Correction با عملیات ریاضی
-background = cv2.blur(img, (51, 51))  # تخمین پس‌زمینه با فیلتر میانگین‌گیر
-corrected = cv2.subtract(img, background)
-corrected = cv2.normalize(corrected, None, 0, 255, cv2.NORM_MINMAX)
-
-# 3. فیلتر (میانگین‌گیر برای کاهش نویز)
+فیلتر (میانگین‌گیر برای کاهش نویز)
 filtered = cv2.medianBlur(corrected, 5)
 
 # 4. Morphology - انتخاب structuring element
